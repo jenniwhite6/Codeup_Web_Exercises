a3c32cbe6c4527e4a5af1a408ad5e61e187821a2
jenniwhite6@gmail.com <jenniwhite6@gmail.com>
2015-04-25 10:29:19 -0500

    Modifications to JS If Else exercise.
    Includes a variable that capitalizes first character of the string

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -1,27 +1,37 @@
 // ignore these lines for now
 // just know that the variable 'color' will end up with a random value from the colors array
+"use strict";
+
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 var color = colors[Math.floor(Math.random()*colors.length)];
 
-var favorite = 'green'; // TODO: change this to your favorite color from the list
+console.log(color);
 
+var favorite = 'blue'; // TODO: change this to your favorite color from the list
+
+var message;
 
 if (color == 'red') {	
-	console.log(color + " is the color of pepperonis.");
+	message = color + ' is the color of pepperonis.';
 } else if (color == 'orange') {
-	console.log(color + " is the color of oranges.");
+	message = color + ' is the color of oranges.';
 } else if (color == 'yellow') {
-	console.log(color + " is da color of da sun, yo.");
+	message = color + ' is da color of da sun, yo.';
 } else if (color == 'green') {
-	console.log(color + " is the color of Codeup.");
+	message = color + ' is the color of Codeup.';
 } else if (color == 'blue') {
-	console.log(color + " is the color of the ocean.");
+	message = color + ' is the color of the ocean.';
 } else {
-	console.log("I do not know anything by that color.")
+	message = 'I do not know anything by that color.';
 }
 
-color == (favorite) ? console.log("Yay! That's my favorite color!") : console.log("Not my favorite color.");
+message = message.charAt(0).toUpperCase() + message.slice(1);
 
+console.log(message);
+
+var message = (favorite == color) ? "Yay! That's my favorite color!" : "Not my favorite color.";
+
+console.log(message);
 
 // TODO: Create a block of if/else statements to check for every color except indigo and violet.
 // TODO: When a color is encountered log a message that tells the color, and an object of that color.
