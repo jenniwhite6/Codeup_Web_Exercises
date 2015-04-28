e17e892957bfd06f72262cab365d291cb017a83e
jenniwhite6@gmail.com <jenniwhite6@gmail.com>
2015-04-27 20:08:17 -0500

    JS exercises with loops and functions

--- public/js/if_else.js
+++ public/js/if_else.js
@@ -1,5 +1,3 @@
-// ignore these lines for now
-// just know that the variable 'color' will end up with a random value from the colors array
 "use strict";
 
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -7,7 +5,7 @@ var color = colors[Math.floor(Math.random()*colors.length)];
 
 console.log(color);
 
-var favorite = 'blue'; // TODO: change this to your favorite color from the list
+var favorite = 'blue'; 
 
 var message;
 
@@ -32,13 +30,3 @@ console.log(message);
 var message = (favorite == color) ? "Yay! That's my favorite color!" : "Not my favorite color.";
 
 console.log(message);
-
-// TODO: Create a block of if/else statements to check for every color except indigo and violet.
-// TODO: When a color is encountered log a message that tells the color, and an object of that color.
-//       Example: Blue is the color of the sky.
-
-// TODO: Have a final else that will catch indigo and violet.
-// TODO: In the else, log: I do not know anything by that color.
-
-// TODO: Using the ternary operator, conditionally log a statement that
-//       says whether the random color matches your favorite color.
