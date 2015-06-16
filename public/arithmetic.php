bf4c4d7f5aa93b69fa0a05369a2fba6743ec76b3
jenniwhite6@gmail.com <jenniwhite6@gmail.com>
2015-06-15 19:34:59 -0500

    Arithmetic PHP assignment - raw

---
+++ public/arithmetic.php
@@ -0,0 +1,33 @@
+<?php
+
+function add($a, $b)
+{
+    echo $a + $b;
+}
+
+function subtract($a, $b)
+{
+    // Add code here
+}
+
+function multiply($a, $b)
+{
+    // Add code here
+}
+
+function divide($a, $b)
+{
+    // Add code here
+}
+
+// Add code to test your functions here
+
+
+// Add a function modulus that finds the modulus of 2 numbers.
+// Add test code and verify the output of modulus.
+// Create variables $a and $b at the top of your script
+//  and give them different values. Watch what happens
+//   (or, does not happen) to your function output as
+//    you set and modify $a & $b outside of your functions. 
+//    Think carefully about how this behavior differs from 
+//    the way JavaScript handles variables.
