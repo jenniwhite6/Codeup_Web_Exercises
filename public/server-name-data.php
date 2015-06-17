03ef8f435e41793b4e4ad7b932cd19944bc0354e
jenniwhite6@gmail.com <jenniwhite6@gmail.com>
2015-06-16 19:52:09 -0500

    Separated out the data from Server Name Gen into a new PHP file to be included above HTML

---
+++ public/server-name-data.php
@@ -0,0 +1,43 @@
+<?php 
+
+	$nouns = [
+		'mug',
+		'elephant',
+		'printer',
+		'chair',
+		'thermos',
+		'hat',
+		'squirrel',
+		'pencil',
+		'cable',
+		'machine',
+		'pencil',
+		'silverware',
+		'doorway',
+		'window',
+		'blob',
+		'bowl-cut'
+	];
+
+	$adjectives = [
+		'didactic',
+		'fastidious',
+		'prolific',
+		'luminous',
+		'obtuse',
+		'quiescent',
+		'serpentine',
+		'turbulent',
+		'awesome',
+		'zealous',
+		'competitive',
+		'diagonal',
+		'curvacious',
+		'metallic',
+		'ceramic',
+		'speedy',
+		'rubberized',
+		'zany'
+	];
+
+?>
