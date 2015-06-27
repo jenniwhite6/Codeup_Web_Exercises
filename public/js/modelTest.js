f1522313dca52af09cb2254ae01148ade778d398
jenniwhite6@gmail.com <jenniwhite6@gmail.com>
2015-06-27 13:54:05 -0500

    Cleaning up model test exercise + added an example controller to the JS file for later reference

--- public/js/modelTest.js
+++ public/js/modelTest.js
@@ -2,7 +2,17 @@
 	"use strict";
 		var app = angular.module('modelTest', []);
 
-		app.controller("ExampleController", function() {
+		app.controller('ExampleController', function() {
 			
+			this.users = [];
+
+			this.newUser = {};
+
+			this.saveUser = function() {
+				// push newUser to users array + reset newUser
+				this.users.push(this.newUser);
+				this.newUser = {};
+			};
 		});
+
 })();
